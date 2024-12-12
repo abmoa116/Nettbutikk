c173b5a9a724ed1ee9b7545777c5a17cf07361e1
abmoa116 <168414487+abmoa116@users.noreply.github.com>
2024-12-12 11:30:13 +0100

    Initial commit

---
+++ Controllers/NewUser Controller1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class NewUser_Controller1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
